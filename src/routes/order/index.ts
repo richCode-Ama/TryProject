7d9997a443f60dc924e447fc81fe77fecc9323df
richCodeAma <richard.appiah@amalitech.com>
2022-07-26 21:04:56 +0000

    fixing route issues

--- src/routes/order/index.ts
+++ src/routes/order/index.ts
@@ -5,9 +5,10 @@ import orderController from '../../controllers/order/order_controller';
 const orderRouter =  express.Router();
 
 orderRouter.post('/createOrder', orderController.createOrder )
-orderRouter.put('/updateOrder', orderController.updateOrder )
-orderRouter.delete('/deleteOrder', orderController.DeleteOrder )
-orderRouter.get('/', orderController.getOrder)
+orderRouter.put('/updateOrde:orderId', orderController.updateOrder )
+orderRouter.delete('/:orderId', orderController.DeleteOrder )
+orderRouter.post('/:orderId', orderController.getOrder)
 
 
 export default orderRouter;
+
