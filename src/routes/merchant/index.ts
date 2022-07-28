f06376705540f14766b28e693bd15a165587e973
richCodeAma <richard.appiah@amalitech.com>
2022-07-28 09:38:29 +0000

    add alll merchant and pizza  route

--- src/routes/merchant/index.ts
+++ src/routes/merchant/index.ts
@@ -6,6 +6,8 @@ import merchantController from '../../controllers/merchant/merchant_controller';
 const merchantRouter =  express.Router();
 
 merchantRouter.post('/seedMerchantAndPizza', merchantController.seedMerchantAndPizza )
+merchantRouter.get("/getAllPizza", merchantController.getAllPizza)
+merchantRouter.get("/getAllMerchant", merchantController.getAllMerchant)
 
 
 export default merchantRouter;
